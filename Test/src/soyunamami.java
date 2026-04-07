60b7fa6a28629dee90da7821849b17835a1999ec
alexmaho920-boo <alexmaho920@gmail.com>
2026-04-06 23:00:03 -0600

    hola

--- Test/src/soyunamami.java
+++ Test/src/soyunamami.java
@@ -2,5 +2,6 @@ public class soyunamami {
     public static void main(String[] args) {
         System.out.println("soy una mami");
         System.out.println("si we");
+        System.out.println("Eres puto we");
     }
 }
